407dd5ec85385a5ca30b0f951300272209edb3e1
Chad Drake <chaddrake1313@yahoo.com>
2020-08-31 13:19:14 -0500

    making progress

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { Component } from "react";
+import Form from "./Form";
 
-function App() {
-  return <main className="App">{/* content goes here */}</main>;
+class App extends Component {
+  state = {};
+
+  getList() {
+    fetch();
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Google Books</h1>
+        <main>
+          <Form clickList={this.getList} />
+        </main>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ src/Form.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Form(props) {
+  return (
+    <div className="form">
+      <form>
+        <label htmlFor="search-terms">Search Terms</label>
+        <input type="text" id="search-terms" name="search-terms" />
+
+        <label htmlFor="filter">Filter</label>
+        <select id="filter" name="filter">
+          <option value="all">All</option>
+          <option value="partial">Partial</option>
+          <option value="full">Full</option>
+          <option value="free-ebooks">Free Ebooks</option>
+          <option value="paid-ebooks">Paid Ebooks</option>
+          <option value="ebooks">Ebooks</option>
+        </select>
+
+        <button onClick={() => props.clickList()}> Search </button>
+      </form>
+    </div>
+  );
+}
